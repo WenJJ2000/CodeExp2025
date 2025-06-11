2c867817ead959b92c8ec61c1be273e7c546b40b
Jett-Tan <68832631+Jett-Tan@users.noreply.github.com>
2025-06-11 10:45:00 +0800

    Add notification functionality: create NotificationPage, update BellNotification, and enhance notification context

--- components/custom-ui/home/bell-notification.tsx
+++ components/custom-ui/home/bell-notification.tsx
@@ -1,12 +1,24 @@
 import { FontAwesome6 } from "@expo/vector-icons";
 import { Pressable, View } from "react-native";
 import { Badge } from "~/components/ui/badge";
+import { Text } from "~/components/ui/text";
+import { useRouter } from "expo-router";
+import { useNotification } from "~/lib/useContext/useNotificationContext";
 
 export function BellNotification() {
+  const router = useRouter();
+
+  const { notifications } = useNotification();
   return (
-    <Pressable>
-      <Badge variant="outline" className="p-4">
-        <FontAwesome6 name="bell" />
+    <Pressable onPress={() => router.push("/notificationPage")}>
+      <View className="w-18 h-18 border border-input bg-background p-4 rounded-full justify-center items-center ">
+        <FontAwesome6 name="bell" className="" />
+      </View>
+      <Badge
+        variant="default"
+        className=" items-center justify-center absolute -top-1 -right-1 p-1 w-6 h-6"
+      >
+        <Text className="text-xs">{notifications.length}</Text>
       </Badge>
     </Pressable>
   );
