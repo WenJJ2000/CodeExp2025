9e1a52d5d9e0679d0f249801bcf113a8c41702e3
Jett-Tan <68832631+Jett-Tan@users.noreply.github.com>
2025-06-11 15:29:23 +0800

    Refactor BellNotification and HomeHeader: update to use TouchableOpacity, improve styling, and display user profile in HomeHeader; fix profile picture key in AuthApi

--- components/custom-ui/home/home-header.tsx
+++ components/custom-ui/home/home-header.tsx
@@ -2,14 +2,38 @@ import { Text } from "~/components/ui/text";
 import AppHeader from "../app-header";
 import { BellNotification } from "./bell-notification"; // Adjust the import path as necessary
 import { useNotification } from "~/lib/useContext/useNotificationContext";
+import { useAuth } from "~/lib/useContext/useAuthContext";
+import { SettingsButton } from "~/components/settingsButton";
+import { ThemeToggle } from "~/components/ThemeToggle";
+import { useRouter } from "expo-router";
+import { Image, Pressable } from "react-native";
 export default function HomeHeader() {
   const { notifications } = useNotification();
+  const { user } = useAuth();
+  const router = useRouter();
   return (
     <AppHeader
-      leftChildren={<Text className="text-2xl font-bold ">Home</Text>}
+      leftChildren={
+        <>
+          <Pressable onPress={() => router.push("/(tabs)/(profile-tabs)")}>
+            <Image
+              source={{ uri: `data:image/jpeg;base64,${user?.profilePicture}` }}
+              className="w-12 h-12 rounded-full mr-3 border-2 border-gray-300 ml-4"
+            />
+          </Pressable>
+          <Text className="text-2xl font-bold ">Welcome {user?.username}!</Text>
+        </>
+      }
       rightChildren={
         <>
+          <ThemeToggle />
           <BellNotification />
+          <SettingsButton
+            onPress={() => {
+              router.push("/(tabs)/(profile-tabs)/settings");
+            }}
+            style={{ marginLeft: 10 }}
+          />
         </>
       }
     />
--- components/settingsButton.tsx
+++ components/settingsButton.tsx
@@ -1,34 +1,41 @@
 import { Ionicons } from "@expo/vector-icons";
-import { StyleProp, TouchableOpacity, ViewStyle } from "react-native";
+import {
+  Pressable,
+  StyleProp,
+  TouchableOpacity,
+  View,
+  ViewStyle,
+} from "react-native";
 import { useColorScheme } from "~/lib/useColorScheme";
 
 export function SettingsButton({
-    onPress,
-    style,
+  onPress,
+  style,
 }: {
-    onPress: () => void;
-    style?: StyleProp<ViewStyle>;
+  onPress: () => void;
+  style?: StyleProp<ViewStyle>;
 }) {
-    const { isDarkColorScheme } = useColorScheme();
+  const { isDarkColorScheme } = useColorScheme();
 
-    const iconColor = isDarkColorScheme ? "white" : "black";
+  const iconColor = isDarkColorScheme ? "white" : "black";
 
-    const backgroundColor = isDarkColorScheme ? "#2f2f2f" : "#FFFFFF"; // dark grey vs light grey
+  const backgroundColor = isDarkColorScheme ? "#2f2f2f" : "#FFFFFF"; // dark grey vs light grey
 
-    return (
-        <TouchableOpacity
-            onPress={onPress}
-            activeOpacity={0.7}
-            style={[
-                {
-                    backgroundColor,
-                    borderRadius: 9999, // rounded full
-                    padding: 6, // comfortable tap area
-                },
-                style,
-            ]}
-        >
-            <Ionicons name="settings-outline" size={25} color={iconColor} />
-        </TouchableOpacity>
-    );
+  return (
+    <Pressable
+      onPress={onPress}
+      // activeOpacity={0.7}
+      style={[
+        {
+          backgroundColor,
+          borderRadius: 9999, // rounded full
+          //   padding: 6, // comfortable tap area
+        },
+        style,
+      ]}
+      className="w-18 h-18 border border-input bg-background p-4 rounded-full justify-center items-center "
+    >
+      <Ionicons name="settings-outline" color={iconColor} size={14} />
+    </Pressable>
+  );
 }
