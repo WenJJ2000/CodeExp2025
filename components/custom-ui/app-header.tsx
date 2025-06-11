d0a0c3660c732cac7fd331b10f9cba9b2f0c2ffe
Jett-Tan <68832631+Jett-Tan@users.noreply.github.com>
2025-06-12 00:42:59 +0800

    fix: adjust width of leftChildren container in AppHeader for better alignment

--- components/custom-ui/app-header.tsx
+++ components/custom-ui/app-header.tsx
@@ -9,7 +9,7 @@ export default function AppHeader({
 }) {
   return (
     <View className="w-full flex-row justify-between items-center h-20 border-b-secondary px-4 py-4">
-      <View className="flex-row justify-between items-center gap-2 w-[80px]">
+      <View className="flex-row justify-between items-center gap-2 w-[100px]">
         {leftChildren}
       </View>
       <View className="flex-row items-center justify-end gap-2 w-[200px]">
