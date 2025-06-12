df66f4067ec4005ecdfbca3624fd9f4418ac20ae
Jett-Tan <68832631+Jett-Tan@users.noreply.github.com>
2025-06-12 10:05:56 +0800

    refactor: improve live update handling and simplify marker rendering in HomeMap component

--- components/custom-ui/home/home-map.tsx
+++ components/custom-ui/home/home-map.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import MapView, { Marker } from "react-native-maps";
 import { liveUpdate } from "~/firebase/ForumApi";
 import { ScamReport } from "~/lib/types";
-import { View } from "react-native";
 import { useRouter } from "expo-router";
 import * as Location from "expo-location";
 
@@ -10,12 +9,15 @@ export default function HomeMap() {
   const [scamReports, setScamReports] = useState<ScamReport[]>([]);
   const router = useRouter();
   useEffect(() => {
-    liveUpdate((data) => {
+    const unsub = liveUpdate((data) => {
       setScamReports(
         data.filter((report) => report.scamReportType == "IN_PERSON")
       );
     });
-  }, []);
+    return () => {
+      unsub();
+    };
+  }, [scamReports]);
 
   const [location, setLocation] = useState<Location.LocationObject | null>(
     null
@@ -38,49 +40,49 @@ export default function HomeMap() {
   return (
     <MapView
       style={{ width: "100%", height: "40%" }}
-      region={
-        location
-          ? {
-              latitude: location.coords.latitude,
-              longitude: location.coords.longitude,
-              latitudeDelta: 0.015,
-              longitudeDelta: 0.015,
-            }
-          : undefined
-      }
+      region={{
+        latitude: location?.coords?.latitude || 1.3521,
+        longitude: location?.coords?.longitude || 103.8198,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.015,
+      }}
     >
       {scamReports.map((report) => {
-        if (report.location !== undefined && report.location !== null) {
-          return (
-            <Marker
-              key={report.id}
-              coordinate={{
-                latitude: report.location.latitude,
-                longitude: report.location.longitude,
-              }}
-              title={report.title}
-              description={`Posted by ${report.createdBy.username}`}
-              pinColor={
-                report.scamReportStatus === "VALID"
-                  ? "green"
-                  : report.scamReportStatus === "INVALID"
-                  ? "red"
-                  : "blue"
-              }
-              style={{
-                width: 50,
-                height: 50,
-              }}
-              onPress={() => {
-                router.push({
-                  pathname: "/forumPage",
-                  params: { scamReportId: report.id },
-                });
-              }}
-            />
-          );
-        }
-        return <></>; // Skip markers without location
+        // console.log("Report");
+        // console.log(
+        //   "Report",
+        //   report.id,
+        //   report?.location?.latitude,
+        //   report?.location?.longitude
+        // );
+        return (
+          <Marker
+            key={report.id}
+            coordinate={{
+              latitude: report?.location?.latitude || 0,
+              longitude: report?.location?.longitude || 0,
+            }}
+            title={report.title}
+            description={`Posted by ${report.createdBy.username}`}
+            pinColor={
+              report.scamReportStatus === "VALID"
+                ? "green"
+                : report.scamReportStatus === "INVALID"
+                ? "red"
+                : "blue"
+            }
+            style={{
+              width: 50,
+              height: 50,
+            }}
+            onPress={() => {
+              router.push({
+                pathname: "/forumPage",
+                params: { scamReportId: report.id },
+              });
+            }}
+          />
+        );
       })}
     </MapView>
   );
