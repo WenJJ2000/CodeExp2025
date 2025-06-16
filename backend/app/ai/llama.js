92c79bd394e6fe7611e96f4dc73a6816fa9ada1f
wenjj2000 <wenjj2000@gmail.com>
2025-06-16 12:26:58 +0800

    hotfix

--- backend/app/ai/llama.js
+++ backend/app/ai/llama.js
@@ -1,55 +1,18 @@
-import OpenAI from "openai";
+import OpenAI from 'openai';
 
-// export async function checkImageForScam(image) {
-//   try {
-//     const data = await fetch('https://openrouter.ai/api/v1/chat/completions', {
-//       method: 'POST',
-//       headers: {
-//         Authorization: `Bearer ${process.env.OPEN_ROUTER_API_KEY}`, // Replace with your OpenRouter API key
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify({
-//         model: 'meta-llama/llama-3.3-8b-instruct:free',
-//         messages: [
-//   {
-//     role: 'user',
-//     content: [
-//       {
-//         type: 'text',
-//         text: `I am a scam detective. I am trying to find out whether this image is a scam,
-//                         Generate a report on the image given and give me all the details and parts on the image which may
-//                         have been modified or AI generated. This should include any anomaly no matter how small
-//                         Include a confidence level on how likely it is a scam.`,
-//       },
-//       {
-//         type: 'image_url',
-//         url: image,
-//       },
-//     ],
-//   },
-//         ],
-//       }),
-//     });
-//     return data;
-//   } catch (error) {
-//     console.log(error);
-//     throw new Error('Failed to fetch ai');
-//   }
-// }
 const openai = new OpenAI({
-  baseURL: "https://openrouter.ai/api/v1",
-  apiKey:
-    "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",
+  baseURL: 'https://openrouter.ai/api/v1',
+  apiKey: process.env.OPEN_ROUTER_API_KEY,
 });
 export async function checkImageForScam(image, description) {
   const completion = await openai.chat.completions.create({
-    model: "meta-llama/llama-3.3-8b-instruct:free",
+    model: 'meta-llama/llama-3.3-8b-instruct:free',
     messages: [
       {
-        role: "user",
+        role: 'user',
         content: [
           {
-            type: "text",
+            type: 'text',
             text: `I am a scam detective. I am trying to find out whether this image is a scam,
                     Generate a report on the image given and give me all the details and parts on the image which may
                     have been modified or AI generated. This should include any anomaly no matter how small
@@ -57,11 +20,11 @@ export async function checkImageForScam(image, description) {
                     {isScam: boolean, reasons: string[], confidence: number} and nothing else`,
           },
           {
-            type: "image_url",
+            type: 'image_url',
             url: image,
           },
           {
-            type: "text",
+            type: 'text',
             text: `Here is a description of the image as well to help aid in your detection : ${description}`,
           },
         ],
@@ -73,15 +36,15 @@ export async function checkImageForScam(image, description) {
 }
 export async function generateTitleAndDescription(description) {
   const completion = await openai.chat.completions.create({
-    model: "meta-llama/llama-3.3-8b-instruct:free",
-    model: "mistralai/devstral-small:free",
+    model: 'meta-llama/llama-3.3-8b-instruct:free',
+    model: 'mistralai/devstral-small:free',
     messages: [
       {
-        role: "user",
+        role: 'user',
         content:
-          "Generate a concise title and description for a scam report based on the following description:\n\n" +
+          'Generate a concise title and description for a scam report based on the following description:\n\n' +
           description +
-          "\n\n If there is a phone number or email address, it should be in the title. Please provide the title and description in the following JSON format:\n\n" +
+          '\n\n If there is a phone number or email address, it should be in the title. Please provide the title and description in the following JSON format:\n\n' +
           `{"title": "Your Title Here", "description": "Your Description Here"}`,
       },
     ],
